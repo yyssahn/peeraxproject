bb50340a145789ac59dfc404d4a12b74923b33c0
Yoonsung Ahn <yyssahn@hotmail.com>
2014-07-27 19:49:27 -0700

    fking ui minor minor revision

--- src/com/ys/peeraxproject/HomePageActivity.java
+++ src/com/ys/peeraxproject/HomePageActivity.java
@@ -1,7 +1,9 @@
 package com.ys.peeraxproject;
 
 
+import com.ys.peeraxproject.ViewSubjectsActivity.SubjectAdapter;
 import com.ys.peeraxproject.helper.DatabaseHandler;
+import com.ys.peeraxproject.helper.JSONParser;
 
 import android.app.ActionBar;
 import android.app.Activity;
@@ -27,9 +29,11 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -37,45 +41,61 @@ import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import view.PeopleListItem;
+import view.SubjectListItem;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class HomePageActivity extends Activity {
 
-    Button homeBtn;
-    Button proBtn;
-    Button conBtn;
-    Button sesBtn;
-    Button usrBtn;
-    Button logoutBtn;
     LayoutInflater inflater;
     DatabaseHandler db;
     Dialog dil;
-
+    ListView peopleList;
     static Bitmap profile_picture;
     static String user_name;
+	private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
+    private static String KEY_ERROR_MSG = "error_msg";
+    private static String KEY_UID = "unique_id";
+    private static String KEY_NAME = "name";
+    private static String KEY_EMAIL = "email";
+    private static final String KEY_ID = "uid";
+    private static final String KEY_ABOUT = "about";;
+    private static final String KEY_DEGREE = "degree";;
+    private static String register_tag = "register";
+
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
+	private static final String TAG_CRITERIA = "criteria";
+	private static final String TAG_SUBJECT = "subject";
+	private String criteria;
+	private String subject;
+	ArrayList<HashMap<String, String>> pList;
+    
+    private static String getpeopleURL = "http://104.131.141.54/lny_project/get_people.php";
+    JSONParser jsonParser =new JSONParser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.tempmainscreen);
-        homeBtn = (Button) findViewById(R.id.temphomebtn);
-        proBtn = (Button) findViewById(R.id.tempprofilebtn);
-        conBtn = (Button) findViewById(R.id.tempconversationbtn);
-        sesBtn = (Button) findViewById(R.id.tempsessionbtn);
-        usrBtn = (Button) findViewById(R.id.tempuserbtn);
-        logoutBtn = (Button) findViewById(R.id.templogout);
-        db = new DatabaseHandler(getApplicationContext());
-
+         db = new DatabaseHandler(getApplicationContext());
+         peopleList = (ListView) findViewById(R.id.peoplelist);
+         pList = new ArrayList<HashMap<String, String>>();
+         Log.d("shit","eat");
+         new GetPeople().execute();
 		/*
 		if (db.getRowCount()==0){
         	Intent i = new Intent(HomePageActivity.this, StartScreenActivity.class);
@@ -84,42 +104,6 @@ public class HomePageActivity extends Activity {
 
         }
 		 */
-        homeBtn.setOnClickListener(new OnClickListener(){
-
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                Intent i = new Intent(HomePageActivity.this, HomePageActivity.class);
-                startActivity(i);
-            }
-
-        });
-
-        proBtn.setOnClickListener(new OnClickListener(){
-
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                Intent i = new Intent(HomePageActivity.this, ProfilePageActivity.class);
-                startActivity(i);
-                //	finish();
-            }
-
-        });
-
-
-        logoutBtn.setOnClickListener(new OnClickListener(){
-
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                db.resetTables();
-                Intent i = new Intent(HomePageActivity.this, StartScreenActivity.class);
-                startActivity(i);
-                //	finish();
-            }
-
-        });
 
     }
 
@@ -176,8 +160,8 @@ public class HomePageActivity extends Activity {
 		final RelativeLayout layout = (RelativeLayout) findViewById(R.id.actionbar);
 		layout.setBackgroundResource(R.drawable.optionbar1);
 		final Button actionBarBack = (Button) findViewById(R.id.optionbackbutton);
-        actionBarBack.setBackgroundResource(R.drawable.optionpillbg);
-        actionBarBack.setText("Refresh");
+        actionBarBack.setBackgroundResource(R.drawable.pill2);
+       
 		
 		actionBarBack.setOnClickListener(new OnClickListener(){
 
@@ -193,8 +177,8 @@ public class HomePageActivity extends Activity {
 		});
 
 		final Button actionBarInflate = (Button) findViewById(R.id.createoptionbutton);
-        actionBarInflate.setBackgroundResource(R.drawable.optionpillbg);
-        actionBarInflate.setText("Menu");
+        actionBarInflate.setBackgroundResource(R.drawable.pill);
+      
 
 		actionBarInflate.setOnClickListener(new OnClickListener(){
 
@@ -305,4 +289,142 @@ public class HomePageActivity extends Activity {
             return super.onOptionsItemSelected(item);
         }
     }
+	
+	class PeopleAdapter extends BaseAdapter{
+
+		ArrayList<HashMap<String, String>> list;
+		public PeopleAdapter(ArrayList<HashMap<String, String>> pList) {
+			// TODO Auto-generated constructor stub
+			list = pList;
+		}
+
+		//ArrayList<HashMap<String,String>> slist;
+		//SubjectAdapter(ArrayList<HashMap<String, String>> subjectList){
+		//	this.slist = subjectList;
+		//}
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return list.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+			
+		}
+ 
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+		
+//			SubjectListItem list = new SubjectListItem(getApplicationContext(),sList.get(arg0).get("criteria"), sList.get(arg0).get("subject"),sList.get(arg0).get("price"), db.getPhoneNumber());
+//			list.setText1(pList.get(arg0).get("subject"));
+
+//			list.setText2(pList.get(arg0).get("price"));
+			PeopleListItem pitem = new PeopleListItem(getApplicationContext());
+			pitem.setAbout(list.get(arg0).get("about"));
+			pitem.setName(list.get(arg0).get("name"));
+			pitem.setPhonenumber(list.get(arg0).get("phonenumber"));
+			return pitem;
+		
+		}
+		
+	}
+	
+	class GetPeople extends AsyncTask<String, String, String> {
+		 
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            Log.d("shit","eat2");
+        	super.onPreExecute();
+         }
+ 
+        /**
+         * Creating product
+         * */
+        @Override
+		protected String doInBackground(String... args) {
+//        	 Log.d("some error", email);
+//            Log.d("some error", password);
+            Log.d("shit","eat3");
+            JSONArray peoplearray = null;
+            
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("tag", "about"));
+           // getting JSON Object
+           JSONObject json = jsonParser.makeHttpRequest(getpeopleURL,
+                    "POST", params);
+            // check log cat fro response
+            Log.d("Create Response", json.toString());
+ 
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+ 
+                if (success == 1) {
+              		
+                    //finish();
+                	peoplearray = json.getJSONArray("people");
+                	
+                    // looping through All Products
+    
+                	for (int i = 0; i < peoplearray.length(); i++) {
+                        JSONObject c = peoplearray.getJSONObject(i);
+ 
+                        // Storing each json item in variable
+                        String name = c.getString("name");
+                        String about = c.getString("about");
+                        String phonenumber = c.getString("phonenumber");
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+ 
+                        // adding each child node to HashMap key => value
+                        map.put("about", about);
+                        map.put("phonenumber", phonenumber);
+                        map.put("name", name);
+                        // adding HashList to ArrayList
+                        pList.add(map);
+                    }
+               } else {
+                    // failed to create product
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+ 
+            return null;
+        }
+ 
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        @Override
+		protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+        //    pDialog.dismiss();
+        	 runOnUiThread(new Runnable() {
+                 public void run() {
+                     /**
+                      * Updating parsed JSON data into ListView
+                      * */
+                	 PeopleAdapter adapter = new PeopleAdapter(pList);
+                 	peopleList.setAdapter(adapter);
+                 	
+                      }
+             });
+        	}
+
+	}
 }
---
+++ src/view/PeopleListItem.java
@@ -0,0 +1,113 @@
+package view;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.ys.peeraxproject.ProfilePageActivity;
+import com.ys.peeraxproject.R;
+import com.ys.peeraxproject.ViewSubjectsActivity;
+import com.ys.peeraxproject.helper.JSONParser;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PeopleListItem extends LinearLayout {
+
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_ABOUT = "about";
+	private static final String TAG_SUBJECT = "subject";
+	private static final String TAG_DEGREE = "degree";
+	JSONParser jsonParser = new JSONParser();
+	private static String loginURL = "http://104.131.141.54/lny_project/delete_subject.php";
+	private static String info_tag = "info";
+    private static final int GET_LOCAL_IMAGE = 8;
+	TextView abouttext;
+	TextView nametext;
+	TextView pricetext;
+	ImageView image;
+	String pnumber;
+	String price;
+	String criteria;
+	String subject;
+	String phonenumber;
+
+    static Bitmap profile_picture;
+	public PeopleListItem(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		init(context);
+	}
+
+	private void init(Context con) {
+		// TODO Auto-generated method stub
+		LayoutInflater inflater = (LayoutInflater) con.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.people, this, true);
+		abouttext= (TextView) findViewById(R.id.peopleabout);
+		nametext=(TextView) findViewById(R.id.peoplename);
+		image = (ImageView)findViewById(R.id.pid);
+	}
+	
+	public void setAbout(String str){
+		abouttext.setText(str);
+	}
+	
+	public void setName(String str){
+		nametext.setText(str);
+	}
+	public void setPhonenumber(String str){
+		phonenumber =str;
+		new GetInfo().execute();
+		
+	}
+	class GetInfo extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            try {
+                profile_picture = BitmapFactory.decodeStream((InputStream) new URL("http://104.131.141.54/lny_project/" + phonenumber + ".jpg").getContent());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String file_url) {
+            if(profile_picture != null) {
+                image.setImageBitmap(profile_picture);
+            }
+
+        }
+    }
+	
+}
